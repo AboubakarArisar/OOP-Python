c97f98b79f1aa34019d0066643cef4357c1ac193
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-11 01:06:47 +0500

    creating class in python

---
+++ Classes/class.py
@@ -0,0 +1,13 @@
+#classes in python
+
+#creating a class
+
+class student:
+    name = "Aboubakar"
+
+
+#creating an object of the class
+s1 = student()
+
+#accessing the class attribute
+print(s1.name)
